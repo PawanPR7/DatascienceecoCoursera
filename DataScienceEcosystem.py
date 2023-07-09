1d4ab3d6a543899b8661a053e437df95419dead5
PawanPR7 <138717738+PawanPR7@users.noreply.github.com>
2023-07-09 16:51:47 +0530

    Jupyter notebook added

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science tools and ecosystems are summarised
+
+# **Objectives**: Here we will be noting down some of the objectives covered in this notebook
+# * List some important languages used by Data Scientists
+# * List some important tools used by Data Scientists
+# * List some important libraires used by Data Scientists
+
+# Some of the important languages that Data Scientists use are:
+# 1. Pyhton
+# 2. R
+# 3. Java
+# 4. Julia
+# 5. SQL
+
+# Some of the commonly used libraries by Data Scientists are:
+# 1. Tensorflow
+# 2. Numpy
+# 3. Pandas
+# 4. Scipy
+
+#  |Data Science Tools|
+#  |------------------|
+#  | Apache spark   |
+#  | R Studio      |
+#  | Anaconda     |
+#  | Hadoop|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+#This is a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[4]:
+
+
+#This will convert 200 minutes in hours by dividing by 60
+200/60
+
+
+# ## Author
+# Pawan Ramotra
+
+# In[ ]:
+
+
+
+
